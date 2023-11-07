1ef210f1f37cf2697e331e7ed92af3c4600dc4f1
merak48763 <99465512+merak48763@users.noreply.github.com>
2023-11-07 23:01:11 +0800

    Added dark mode control

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useChrononInfo } from "./data/chrononInfo";
 import { CardList } from "./components/cardList";
 import { LoadingDialog } from "./components/loadingDialog";
 import { StarFilter, SeriesFilter } from "./components/filter";
+import { Fab, Tooltip } from "@mui/material";
+import { Brightness6Outlined as ThemeIcon } from "@mui/icons-material";
+import { useDarkModeConfig } from "./theme/provider";
 import styled from "@emotion/styled";
 
 const AppWrapper = styled.div`
@@ -14,6 +17,8 @@ const AppWrapper = styled.div`
 `;
 
 function App() {
+  const {toggleDarkMode} = useDarkModeConfig();
+
   const {ready, filter} = useChrononInfo();
   const [starFilter, setStarFilter] = useState([]);
   const [starFilterActive, setStarFilterActive] = useState(false);
@@ -25,16 +30,21 @@ function App() {
     starFilter: (starFilter.length > 0 && starFilterActive) ? starFilter : null,
   }), [seriesFilter, seriesFilterActive, starFilter, starFilterActive, filter]);
 
-  return (
+  return (<>
     <AppWrapper>
       {ready && (<>
         <SeriesFilter value={seriesFilter} onChange={v => setSeriesFilter(v)} active={seriesFilterActive} onToggle={v => setSeriesFilterActive(v)} />
         <StarFilter value={starFilter} onChange={v => setStarFilter(v)} active={starFilterActive} onToggle={v => setStarFilterActive(v)} />
         <CardList filteredCards={filteredCards} />
       </>)}
-      <LoadingDialog open={!ready} />
     </AppWrapper>
-  );
+    <Tooltip placement="left" arrow title="切換主題">
+      <Fab color="primary" sx={{position: "fixed", bottom: 16, right: 16}} onClick={toggleDarkMode}>
+        <ThemeIcon />
+      </Fab>
+    </Tooltip>
+    <LoadingDialog open={!ready} />
+  </>);
 }
 
 export default App;
